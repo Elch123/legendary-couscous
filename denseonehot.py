f5d90f65eb9b58646175ed1fa77856bd8f329970
Elchanan Haas <haas2376@gmail.com>
2019-01-06 18:20:42 +0200

    make invert

--- denseonehot.py
+++ denseonehot.py
@@ -4,13 +4,14 @@ import torch.nn.functional as F
 #print(dir(F))
 hparams={
 'channels':10,
-'lr':.001,
-'batches':500,
+'lr':.005,
+'batches':10000,
 'batch_size':10,
-'blocks':5
+'blocks':3
 }
 #For all these classes, the inverse is the first return, the log determinant is the second
  #HAHA! make sure to be smart with the means I am taking, and not take a mean over the batch dimesion!!!
+ #Sum logdet over channel dimension, don't mean over batch
 class Lin_bidirectional(nn.Module):
     def __init__(self,hparams):
         super().__init__()
@@ -29,21 +30,51 @@ class Prelu(nn.Module):
     def __init__(self,hparams):
         super().__init__()
         self.hparams=hparams
-        self.alphas=nn.Parameter(torch.ones(size=(hparams['channels'],)))
+        self.alphas=nn.Parameter(torch.zeros(size=(hparams['channels'],)))
     def forward(self,x):
-        return F.prelu(x,torch.exp(self.alphas))
+        y=F.prelu(x,torch.exp(self.alphas))
+        return y
     def inverse(self,x):
         inv=F.prelu(x,1/torch.exp(self.alphas))
         logdet=(inv>=0).float()*torch.ones(size=(self.hparams['channels'],))+(inv<0).float()*self.alphas
-        logdet=torch.mean(logdet,dim=1)
+        logdet=torch.sum(logdet,dim=1)
         #print("Prelu logdet "+str(logdet))
         return (inv,logdet)
+class Square(nn.Module):
+    def __init__(self,hparams):
+        super().__init__()
+        self.hparams=hparams
+    def forward(self,x):
+        y=x**2*torch.sign(x).float()
+        #print(y)
+        return y
+    def inverse(self,x):
+        #print(x)
+        inv=torch.abs(x)**(1/2)*torch.sign(x).float()
+        logdet=torch.sum(torch.log(2*torch.abs(inv)),dim=1)
+        #print(torch.mean(logdet))
+        return (inv,logdet)
+class Addone(nn.Module):
+    def __init__(self,hparams):
+        super().__init__()
+        self.hparams=hparams
+    def forward(self,x):
+        y=x+1
+        #print(y)
+        return y
+    def inverse(self,x):
+        #print(x)
+        inv=x-1
+        logdet=0
+        #print(torch.mean(logdet))
+        return (inv,logdet)
+
 class FC_block(nn.Module):
     def __init__(self,hparams):
         super().__init__()
         self.hparams=hparams
         self.lin=Lin_bidirectional(hparams)
-        self.act=Prelu(hparams)
+        self.act=Square(hparams)
     def forward(self,x):
         return self.act(self.lin(x))
     def inverse(self,x):
@@ -55,7 +86,8 @@ class FC_net(nn.Module):
     def __init__(self,hparams):
         super().__init__()
         self.hparams=hparams
-        blocks=[FC_block(hparams) for i in range(hparams['blocks'])]
+        blocks=[FC_block(hparams) for i in range(hparams['blocks'])] #FC_block
+        blocks.append(Lin_bidirectional(hparams))
         print(blocks)
         self.blocks=nn.ModuleList(blocks)
     def forward(self,x):
@@ -64,7 +96,7 @@ class FC_net(nn.Module):
         return x
     def inverse(self,x):
         state=[x,torch.tensor(0.0,requires_grad=True)] #current inverse, log determinant
-        for i in range(self.hparams['blocks']-1,0,-1):
+        for i in range(self.hparams['blocks']-1,-1,-1):
             inv=self.blocks[i].inverse(state[0])
             #print(inv[1])
             state[0]=inv[0]
@@ -74,11 +106,11 @@ def make_normal_batch(size):
     m = torch.distributions.MultivariateNormal(torch.zeros(size), scale_tril=torch.eye(size)) #zero mean, identity covariancm.samplee
     data=m.sample((100,))
     return data
-def avg_negative_log_gaussian_density(data):
+def negative_log_gaussian_density(data):
     #this assumes a mean of zero, and a standard devation of one. The coefficient will probably be important, so I'm keeping that.
     size=data.shape[1]
     m = torch.distributions.MultivariateNormal(torch.zeros(size), scale_tril=torch.eye(size)) #One over sqrt of 2 pi
-    nll=-m.log_prob(data)/size
+    nll=-m.log_prob(data)
     return nll
 net=FC_net(hparams)
 optimizer = torch.optim.SGD(net.parameters(), lr=hparams['lr'], momentum=0.9,nesterov=True)
@@ -86,23 +118,33 @@ x=make_normal_batch(10)
 def make_batch(size):
     dist=torch.distributions.OneHotCategorical(probs=torch.ones(size=(size,)))
     m = torch.distributions.MultivariateNormal(torch.zeros(size), scale_tril=torch.eye(size))
-    target=1*dist.sample((100,))+.01*m.sample((100,))
+    target=0*dist.sample((100,))+torch.abs(1.0*m.sample((100,)))
     return target
-for e in range(hparams['batches']):
-    #10*make_normal_batch(hparams['batch_size'])
-    target=make_batch(hparams['channels'])
-    #print(target)
-    start=net.inverse(target)
-    distloss=avg_negative_log_gaussian_density(start[0])
-    #print("distribution loss " + str(distloss))
-    loss=distloss+start[1]
-    loss=torch.mean(loss)
-    print(loss)
-    net.zero_grad()
-    loss=loss
-    print(loss)
-    loss.backward()
-    torch.nn.utils.clip_grad_norm_(net.parameters(), 10)
-    optimizer.step()
-print(net(make_normal_batch(hparams['batch_size'])))
+def train():
+    for e in range(hparams['batches']):
+        #10*make_normal_batch(hparams['batch_size'])
+        target=make_batch(hparams['channels'])
+        #print(target)
+        start=net.inverse(target)
+        distloss=negative_log_gaussian_density(start[0])/hparams['channels']
+        print("distribution loss " + str(torch.mean(distloss)))
+        jacloss=start[1]/hparams['channels']
+        print("Transformation loss " + str(torch.mean(jacloss)))
+        loss=distloss+jacloss
+        loss=torch.mean(loss)
+        print(loss)
+        net.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(net.parameters(), 10)
+        optimizer.step()
+def verify():
+    batch=make_batch(hparams['batch_size'])
+    passed=net(net.inverse(batch)[0])
+    passedtwo=net.inverse(net(batch))[0]
+    print(torch.mean(batch-passed))
+    print(torch.mean(batch-passedtwo))
+verify()
+#train()
+#print(net(make_normal_batch(hparams['batch_size'])))
+
 #print(net.lin.linweight)
